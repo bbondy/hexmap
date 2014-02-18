9d8b9bbaa70cd9186f5f401388a76ac0d61d5e4f
Brian R. Bondy <netzen@gmail.com>
2014-02-17 22:42:51 -0500

    Get a ascii based hex map working

--- public/static/js/hexmap.js
+++ public/static/js/hexmap.js
@@ -5,28 +5,44 @@
  "use strict";
 
 
-var hexStyle = {
-  color: '#6C6',
-  fontSize: '100px',
-  marginLeft: '-10px',
-  marginTop: '-50px',
-};
- 
-
 define(['react'], function(React) {
 
   console.log('loaded react...');
+  var x = 0, y = 0;
+  var count = 0;
+  var offset = false;
   var HexMap = React.createClass({
     render: function() {
       console.log('rendering hexmap');
-      return (
-        <div style={hexStyle}>&#x2B23;</div>
-      );
-    }
-  }); 
+
+      var hexNodes = [];
+      for (var r = 0; r < this.props.rows; r++) {
+        for (var c = 0; c < this.props.columns; c++) {
+          var hexStyle = {
+            fontSize: this.props.size + 'px',
+            position: 'absolute',
+            transform: 'translate(' + (c * this.props.distX + (r%2 == 0 ? 0 : 28)) + 'px, ' + r * this.props.distY + 'px)'
+          };
+
+          console.log('r: ' + r + ', c: ' + c);
+          hexNodes.push(
+            <div style={hexStyle}>&#x2B22;</div>
+          );
+
+        }
+      }
+
+    return (
+           <div className='commentList'>
+             {hexNodes}
+           </div>
+            );
+    } 
+        
+  });
 
   React.renderComponent(
-      <HexMap/>,
+      <HexMap rows='5' columns='10' distX='55' distY='50' size='70'/>,
     document.getElementById('hexmap')
   );
 
