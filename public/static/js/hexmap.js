8c3778ad5ea7cbcf7e6bff73b6b29cf5e6c84d1a
Brian R. Bondy <netzen@gmail.com>
2014-02-21 22:09:32 -0500

    Attempted div based hex, doesn't look as promising, made unicode one smaller

--- public/static/js/hexmap.js
+++ public/static/js/hexmap.js
@@ -11,7 +11,7 @@ define(['react'], function(React) {
   var x = 0, y = 0;
   var count = 0;
   var offset = false;
-  var HexMap = React.createClass({
+  var HexMapUnicode = React.createClass({
     render: function() {
       console.log('rendering hexmap');
 
@@ -21,7 +21,7 @@ define(['react'], function(React) {
           var hexStyle = {
             fontSize: this.props.size + 'px',
             position: 'absolute',
-            transform: 'translate(' + (c * this.props.distX + (r%2 == 0 ? 0 : 28)) + 'px, ' + r * this.props.distY + 'px)'
+            transform: 'translate(' + (c * this.props.distX + (r%2 == 0 ? 0 : this.props.size / 2 - 2)) + 'px, ' + r * this.props.distY + 'px)'
           };
 
           console.log('r: ' + r + ', c: ' + c);
@@ -41,8 +41,66 @@ define(['react'], function(React) {
         
   });
 
+  var HexMapDiv = React.createClass({
+    render: function() {
+      console.log('rendering hexmap');
+
+      var hexNodes = [];
+      for (var r = 0; r < this.props.rows; r++) {
+        for (var c = 0; c < this.props.columns; c++) {
+
+
+          var divOne = {
+            float: 'left',
+            borderRight: '30px solid #6C6',
+            borderTop: '52px solid transparent',
+            borderBottom: '52px solid transparent'
+          }
+
+          var divTwo = {
+            float: 'left',
+            width: '60px',
+            height: '104px',
+            backgroundColor: '#6C6'
+          }
+
+          var divThree = {
+            float: 'left',
+            borderLeft: '30px solid #6C6',
+            borderTop: '52px solid transparent',
+            borderBottom: '52px solid transparent'
+          }
+
+          var hexStyle = {
+            fontSize: this.props.size + 'px',
+            position: 'absolute',
+            transform: 'translate(' + (c * this.props.distX + (r%2 == 0 ? 0 : 28)) + 'px, ' + r * this.props.distY + 'px)'
+          };
+        
+          hexNodes.push(
+            <div style={hexStyle}>
+            <div style={divOne}></div>
+            <div style={divTwo}></div>
+            <div style={divThree}></div>
+            </div>
+          );
+
+        }
+      }
+
+    return (
+           <div className='commentList'>
+             {hexNodes}
+           </div>
+            );
+    } 
+        
+  });
+
+
   React.renderComponent(
-      <HexMap rows='5' columns='10' distX='55' distY='50' size='70'/>,
+      //<HexMapUnicode rows='5' columns='10' distX='55' distY='50' size='70'/>,
+      <HexMapUnicode rows='15' columns='10' distX='16' distY='14' size='20'/>,
     document.getElementById('hexmap')
   );
 
